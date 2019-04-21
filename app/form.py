2fece5fbb48fbd49d206e15dc71e083e9677244f
udcun <duphungcong@gmail.com>
2019-04-21 23:31:45 +0700

    user profile

--- app/form.py
+++ app/form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
 from app.models import User
 
 
@@ -27,3 +27,8 @@ class RegisterForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email')
+
+
+class EditProfileForm(FlaskForm):
+    about_me = TextAreaField('Aout me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
